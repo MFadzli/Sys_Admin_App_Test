6193e4c49dff169065dc966cc8bb839003b63d20
MFadzli <88931058+MFadzli@users.noreply.github.com>
2021-08-14 22:27:24 +0800

    Delete .sys_adm_test directory

--- .sys_adm_test/rogue.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#finding rogue file and paste the output
-sudo find / -type f -printf "%s\t%p\n" | sort -n | tail -1; find / -type f -printf '%s %p\n' | sort -nr | head -10 |& tee -a rogue_output.txt
-
-#sending the alert email
-echo "This is the message body and contains the message" | mailx -v -r "Metestemail@yahoo.com" -a /rogue_output.txt -s "This is the subject" -S smtp="mail.yahoo.com:587" -S smtp-use-starttls -S smtp-auth=login -S smtp-auth-user="Metestemail@yahoo.com" -S smtp-auth-password="abc123" -S ssl-verify=ignore alarm@animapoint.net 
